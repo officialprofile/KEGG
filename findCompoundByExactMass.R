e4839be26a62caa3656df6cda00bd823e31a7178
officialprofile <75314157+officialprofile@users.noreply.github.com>
2022-04-22 09:04:27 +0200

    Fixed processing the compound name

--- findCompoundByExactMass.R
+++ findCompoundByExactMass.R
@@ -1,10 +1,10 @@
-findCompoundByExactMass <- function(exact_mass){
+findCompoundByExactMass <- function(exact_mass, onlyFirstName = TRUE){
   names <- c()
   link  <- stringr::str_replace('http://rest.kegg.jp/find/compound/replace/exact_mass', 
                                pattern = 'replace', replacement = as.character(exact_mass))
   raw   <- readLines(link)
-  if (raw == ''){
-    return(list('mass' = exact_mass, 'compound' = NA, 'names' = c()))
+  if (length(raw) == 1){
+    return(list('mass' = exact_mass, 'compound' = NA, names = NA))
   }
   else{
     compound <- stringr::str_split(raw, pattern = '\t', simplify = T)[1]
@@ -17,18 +17,22 @@ findCompoundByExactMass <- function(exact_mass){
     
     for (i in from:to){
       if (i < to){
-        buffor <- str_split(raw[2], pattern = " ", simplify = T)
-        buffor <- str_split(buffor, pattern = ";", simplify = T) 
+        buffor <- str_split(raw[i], pattern = " ", simplify = T)
+        buffor <- str_split(buffor[length(buffor)], pattern = ";", simplify = T) 
         names  <- c(names, buffor[length(buffor)-1])
       }
       else{
-        buffor <- str_split(raw[2], pattern = " ", simplify = T)
+        buffor <- str_split(raw[i], pattern = " ", simplify = T)
         names  <- c(names, buffor[length(buffor)])
       }
     }
+  }
+  if (onlyFirstName){
+    names = names[1]
   }
   return(list('mass' = exact_mass, 'compound' = compound, 'names' = names))
 }
 
 # Example
-# findCompoundByExactMass(286.236876)
+# findCompoundByExactMass(108.0687)
+
